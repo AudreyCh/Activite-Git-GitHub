f0b3e8af0783d94b4d24ad124448e627a1cf378f
Audrey <audrey.chone@toulouse.inra.fr>
2016-05-11 18:23:27 +0200

    Ajout de l'affichage des variables pour être sûre que le script fonctionne correctement

--- script.py
+++ script.py
@@ -5,6 +5,9 @@ from random import randint
 torchon = randint(0,1)
 serviette = 4
 
+print(torchon)
+print(serviette)
+
 if torchon + serviette != serviette:
 	print ("on ne mélange pas les torchons et les serviettes")
 else :
