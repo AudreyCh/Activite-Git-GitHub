eb0198f654fbab6ca46332b49898d3793921fc37
Audrey <audrey.chone@toulouse.inra.fr>
2016-05-11 17:50:16 +0200

    Script humoristique

---
+++ script.py
@@ -0,0 +1,14 @@
+from random import randint
+
+torchon = randint(0,1)
+serviette = 4
+
+if torchon + serviette != serviette:
+	print "on ne mélange pas les torchons et les serviettes"
+else :
+	print "nous n'avons que des serviettes"
+
+
+
+
+
